6cdf565de5cf93434df02e9bb9958cf0b5e06bca
qinlinchong <qinlinchong@163.com>
2016-02-16 18:20:06 +0800

    增加webService远程客户端生成测试用例

---
+++ webservice/byjdk-wsimport/src/com/qlc/test/byjdk/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://byjdk.test.qlc.com/")
+package com.qlc.test.byjdk;
---
+++ webservice/byjdk-wsimport/src/com/qlc/test/wsimport/MyServiceClient.java
@@ -0,0 +1,21 @@
+package com.qlc.test.wsimport;
+
+import com.qlc.test.byjdk.MyService;
+import com.qlc.test.byjdk.MyServiceServer;
+
+/**
+ * 使用jdk自带的wsimport命令生成客户端，命令在command文件中
+ * 
+ * @author Linden
+ * @date 2016年2月16日 下午5:58:53
+ */
+public class MyServiceClient {
+	public static void main(String[] args) {
+		/*执行wsimport命令后生成的MyServiceServer类中已经自动生成获得MyService方法
+		 * 可以直接使用而不必像byjdk项目那样去调用服务
+		 */
+		MyServiceServer myServiceServer = new MyServiceServer();
+		MyService myService = myServiceServer.getMyServiceServerPort();
+		System.out.println("服务器响应：" + myService.sayHi("Linden"));
+	}
+}
